5506fe5e93f60d46aa7ae5fbd6a75ff942522f11
kfoksdccd <126300842+kfoksdccd@users.noreply.github.com>
2023-03-31 20:10:48 -0700

    Updated with comment

--- lab_code/apex_code.h
+++ lab_code/apex_code.h
@@ -11,22 +11,33 @@ using namespace std;
 
 class Circle {
 public:
+    // Constructor with (0,0) as origin
     Circle(double radius);
+
+    // Constructor with (x,y) as origin
     Circle(double x, double y, double radius);
+
     double GetRadius() const;
     double GetX() const;
     double GetY() const;
+
+    // Compute the area of the circle
     double GetArea();
+
+    // Return the total number of Circle being instantiated
     static int GetTotal();
 
 private:
-    void          init(double x, double y, double radius);
     double        _radius;
     double        _x;
     double        _y;
     static int    nTotal;
 };
+
+// When two Circles are equal, all of their non-static private data members are equal.
 bool operator==(const Circle& lhs, const Circle& rhs);
+
+// When CircleA is less than CircleB, it means the radius of CircleA is less than the radius of CircleB
 bool operator<(const Circle& lhs, const Circle& rhs);
 
 #endif //APEXLAB_APEX_CODE_H
